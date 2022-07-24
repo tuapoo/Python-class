41811a876a7030de1a939b84ab8f32e9e328ba9e
tuapoo <porndanai.1998@gmail.com>
2022-07-24 09:53:36 +0700

    no message

--- 650631044 quiz2.py
+++ 650631044 quiz2.py
@@ -1,42 +1,74 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jul 23 21:02:15 2022
+Created on Sat Jul 23 13:24:03 2022
 
 @author: tuapoo
 """
 
-A=int(input('How many stick in the pile :\n'))
-print("There are ",A,"stick in the pile" )
-B=input("whats your name :\n" )
-print("My name is : " , B)
-i=1
+import random
+import time
+coin=random.randint(1,2)
 
-while A<=3 :
-  print(B,", how many stick you will take : ")
-  C=int(input())
-  A=A-C
-  if A==1 and i==1 :
-    print("there are  1 stick in the pile")
+
+sitp=int(input("how many stick in the pile\n"))
+print("There are",sitp,"stick in the pile")
+name=input("What is your name :\n")
+print("My name is :",name)
+print("Let random to find who will go first . . .\n ")
+time.sleep(1)
+stake=0
+Btakestick=0
+i=0
+n=0
+if coin == 1 :
+  print("its head,",name,"will go first")
+elif coin ==2 :
+  print("its tail, bot will go first")
+  
+while sitp>0 :
+    #print(sitp,"remain")
+    if coin==1:
+      time.sleep(1)
+      stake=int(input("{} how many stick you will take : ".format(name)))
+      sitp=sitp-stake
+      winner=name,"win"
+      coin=2
+    elif coin==2:
+      Btakestick=random.randint(1,2)
+      print("bot will take :",Btakestick)
+      sitp=sitp-Btakestick
+      winner="bot, try again later"
+      coin =1
+      time.sleep(1)
+    if (Btakestick>=3 or stake>=3):
+      print("you can not take more than 2 sticks!\n")
+      if Btakestick>=3:
+         print("please choose stick again!\n")
+         sitp=sitp+Btakestick
+         coin=2
+      elif stake>=3:
+         print("please choose stick again!\n")
+         sitp=sitp+stake
+         coin=1
+    elif (stake <= 0) and i==1 :
+      print("No you cant take more less than 1 stick")
+      sitp=sitp+stake
+      coin=1
+    elif ((stake or Btakestick)==1 or (stake  or Btakestick)==2) and sitp ==0 :
+      print("the winner is :",winner)
+    elif ((stake or Btakestick) ==2) and sitp <0 :
+      print("You can not take till stick in the pile <0 please choose stick again!\n")
+      time.sleep(1)
+      if stake==2 and coin==2:
+        print("please choose stick again!\n")
+        sitp=sitp+stake
+        coin=1
+      elif Btakestick==2 and coin==1:
+        print("please choose stick again!\n")
+        sitp=sitp+Btakestick
+        coin=2        
+    elif ((stake or Btakestick)==1 or (stake  or Btakestick)==2) and sitp >0:
+       print("There are ",sitp,"in the pile now\n")
     i=i+1
-  elif A==2 and i ==1:
-     print("there are  2 stick in the pile")
-     i=i+1
-  elif A==1 and i ==2:
-     print("there are  1 stick in the pile")
-     i=i+1
-  elif i==3 :
-    print("Ok there is no stick in the pile . You spend 2 times")
-    break
-  elif A ==4 and i==1 :
-     print("No you cant take more less than 1 stick")
-     A=3
-  elif A==-1 and i==2 :
-    print("There are no enough sticks to take")
-    break
-  elif A==0 and i==2 :
-     print("there are no stick left in the pile")
-     break
-  elif A==0 and i==1 :
-    print("you can not take more than 2 sticks!")
-    A=A+C
-
+    
+    
